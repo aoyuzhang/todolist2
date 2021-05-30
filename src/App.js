125938ebcfb2d68aecccbfce7e61b6117966dfaf
aoyuzhang <haoyu.zhang@mail.mcgill.ca>
2021-05-30 13:14:59 -0700

    update filter Lists

--- src/App.js
+++ src/App.js
@@ -11,9 +11,19 @@ const FILTER_MAP = {
   Completed: task => task.completed
 };
 
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
   const [filter, setFilter] = useState('All');
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed = {name === filter}
+      setFilter = {setFilter}
+    />
+  ));
 
   const tasksNoun = tasks.length !==1 ? 'tasks':'task';
   const headingText = `${tasks.length} ${tasksNoun} tasks remaining`;
@@ -65,9 +75,7 @@ function App(props) {
       <h1>TodoMatic</h1>
         <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterList }
       </div>
       <h2 id="list-heading">
         {headingText}
